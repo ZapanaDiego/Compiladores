9330a6424a250149af80c96b6ed2236c3e98568e
carlosperalta <carlosperaltamarreros>
2026-09-22 08:56:52 -0500

    token

--- src/Token.cpp
+++ src/Token.cpp
@@ -1,2 +1,16 @@
 #include "Token.h"
-//--
+#include <vector>
+#include <string>
+
+struct SymbolEntry {
+    std::string name;
+    std::string type;       // Keyword, ID, Number, String, etc.
+    int firstPosition;
+    int occurrences;
+};
+
+struct LexerOutput {
+    std::vector<Token> tokens;
+    std::vector<LexerError> errors;
+    std::vector<SymbolEntry> symbolTable;
+};
